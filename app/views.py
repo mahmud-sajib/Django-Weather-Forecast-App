8e67bf53499276c37ce397500f7dd1d86899a167
mahmud-sajib <shout.mahmud@gmail.com>
2020-02-05 15:11:44 +0600

    Updated the app

--- app/views.py
+++ app/views.py
@@ -2,37 +2,104 @@ from django.shortcuts import render
 from darksky import forecast
 from datetime import date, timedelta, datetime
 
+from ipstack import GeoLookup
+
 # Create your views here.
 
 def home(request):
-    DHAKA = 23.7104, 90.4074
-    #units='si'
+
+    geo_lookup = GeoLookup("41b1afd1f407de24e54e36d54ceca69c")
+    location = geo_lookup.get_own_location()
+    lat = location['latitude']
+    lng = location['longitude']
+    region = location['region_name']
+    
+    CITY = lat, lng
+    
     api_key = '5cd0bb087cf49e0d8503c2dc342f7af1'
     weekday = date.today()
 
-    with forecast(api_key, *DHAKA) as dhaka:        
-        for day in dhaka.daily:
-            day = dict(day = date.strftime(weekday, '%a'),
+    weekly_weather = {}
+
+    hourly_weather = {}
+
+    with forecast(api_key, *CITY) as city:        
+        for day in city.daily:
+            day = dict(
+            day = date.strftime(weekday, '%a'),
             sum = day.summary,
             tempMin = round((day.temperatureMin-32) * 5.0/9.0),
             tempMax = round((day.temperatureMax-32) * 5.0/9.0) 
             )
-            print('{day}: {sum} Temp range: {tempMin} - {tempMax} C'.format(**day))
+            # print('{day}: {sum} Temp range: {tempMin} - {tempMax} C'.format(**day))
             weekday += timedelta(days=1)
+
+            pic = ''
+            summary = ('{sum}'.format(**day).lower())
+
+            if 'rain' in summary:
+                pic = 'rain.png'
+            elif 'cloudy' in summary:
+                pic = 'cloudy.png'
+            elif 'overcast' in summary:
+                pic = 'partly-cloudy.png'
+            elif 'clear' in summary:
+                pic = 'sunny.png'
+            else:
+                pic = 'cloudy.png'
+            
+            weekly_weather.update({
+            '{day}'.format(**day):{
+                'sum':'{sum}'.format(**day),
+                'tempMin':'{tempMin}'.format(**day), 
+                'tempMax':'{tempMax}'.format(**day),
+                'pic':pic  
+            }})
+
+    today = weekly_weather[(date.today().strftime('%a'))]
+    del weekly_weather[(date.today().strftime("%a"))]
+
     
     hour = datetime.now().hour
-    location = forecast(api_key, *DHAKA)
+    location = forecast(api_key, *CITY)
     i = 0
 
+    hour_ = ''
+
     while hour < 24:
-        temp = location.hourly[i].temperature
+
+        temp = round((location.hourly[i].temperature-32)* 5.0/9.0)
+        
+
+        pic = ''
+        summary = location.hourly[i].summary.lower()
+
+        if 'rain' in summary:
+            pic = 'rain.png'
+        elif 'cloudy' in summary:
+            pic = 'cloudy.png'
+        elif 'overcast' in summary:
+            pic = 'partly-cloudy.png'
+        elif 'clear' in summary:
+            pic = 'sunny.png'
+        else:
+            pic = 'cloudy.png'
 
         if hour < 12:
-            print('{}am -- {}temp'.format(hour,temp))
+            hour_ = '{}am'.format(hour)
+            hourly_weather.update({hour_:{'pic':pic,'temp':temp}})
+
+        elif hour == 12:
+            hour_ = '{}pm'.format(hour)
+            hourly_weather.update({hour_:{'pic':pic,'temp':temp}})
+
         else:
-            print('{}pm -- {}temp'.format(hour,temp))
+            hour_ = '{}pm'.format(hour-12)
+            hourly_weather.update({hour_:{'pic':pic,'temp':temp}})
+            
         
         hour += 1
         i += 1
 
-    return render(request,'index.html')
+
+    return render(request, 'index.html', {'weekly_weather':weekly_weather, 'hourly_weather':hourly_weather, 'today':today, 'region':region })
